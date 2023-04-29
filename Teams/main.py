1979b61f72d17e9cd3905bc9029595163618c6c2
Valera6 <91695523+Valera6@users.noreply.github.com>
2023-04-29 01:18:05 +0000

    thdthdh

--- Teams/main.py
+++ Teams/main.py
@@ -74,7 +74,7 @@ def join_meeting(start_time=None):
     chrome_options.add_argument('--ignore-ssl-errors')
     chrome_options.add_argument("--use-fake-ui-for-media-stream")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")
     #chrome_options.add_argument("--no-sandbox")
